ea27377bbcddb8701766f7d31c45e579d83c4eec
PC155\pc155-PC <shimanuki@kyoei-dk.co.jp>
2016-09-22 15:15:50 +0900

    パスワードフォームENTERキー有効化

--- DCS_NEC/DCS_NEC/Password.vb
+++ DCS_NEC/DCS_NEC/Password.vb
@@ -1,10 +1,19 @@
 ﻿Public Class frmPassword
 
     Private Sub frmPassword_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
     End Sub
 
     Private Sub btnPasswordOk_Click(sender As Object, e As EventArgs) Handles btnPasswordOk.Click
+        PasswordCheck()
+    End Sub
+
+    Private Sub txtPassword_KeyDown(sender As Object, e As KeyEventArgs) Handles txtPassword.KeyDown
+        If e.KeyCode = System.Windows.Forms.Keys.Return Then
+            PasswordCheck()
+        End If
+    End Sub
+
+    Private Sub PasswordCheck()
         If frmMain.PassWord = Trim(txtPassword.Text) Then
             frmMain.PassWordFlag = True
             Me.Close()
